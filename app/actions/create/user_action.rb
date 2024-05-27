4c920ecc6f2f387dec8409a144cad133959d887b
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 16:43:29 +0200

    Generated Files

---
+++ app/actions/create/user_action.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Create
+  class UserAction
+
+    def perform
+      # write the logic here
+    end
+
+  end
+end
---
+++ app/actions/destroy/user_action.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Destroy
+  class UserAction
+
+    def perform
+      # write the logic here
+    end
+
+  end
+end
