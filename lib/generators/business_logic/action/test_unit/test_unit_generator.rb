f6878817ced0605d7e31ff3a042550dbd97d3fd6
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 16:17:13 +0200

    Control Passing Arguments

--- lib/generators/business_logic/action/test_unit/test_unit_generator.rb
+++ lib/generators/business_logic/action/test_unit/test_unit_generator.rb
@@ -16,10 +16,10 @@ module BusinessLogic
 
           if options[:component]
             # rails generate business_logic User create name:string email:string --test_framework=test_unit --component=identity --force
-            # i.e. template "action.rb.erb.tt", "test/components/identity/actions/#{file_name}_action.rb"
-            template "action_test.rb.erb.tt", "#{root_file_path}/actions/#{file_name}_action.rb"
+            # i.e. template "action_test.rb.erb.tt", "test/components/identity/actions/#{file_name}_action_test.rb"
+            template "action_test.rb.erb.tt", "#{root_file_path}/actions/#{file_name}_action_test.rb"
           else
-            template "action_test.rb.erb.tt", "test/actions/#{file_name}_test.rb"
+            template "action_test.rb.erb.tt", "test/actions/#{file_name}_action_test.rb"
           end
         end
       end
